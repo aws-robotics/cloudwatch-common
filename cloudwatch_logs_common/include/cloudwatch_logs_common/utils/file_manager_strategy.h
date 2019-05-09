ea0c7c2755b538cb9829cb77dced2294a007c9bc
Tim Robinson <tim@amazon.com>
2019-05-09 14:28:47 -0700

    Add reading, discovery and rotation to FileManagerStrategy
    - Add function getFileToRead that returns the best file to start reading
    from when sending data from the file to CloudWatch.
    - Add function discoverStoredFiles which finds all files in the
    storage directory and keeps them, in case there were offline logs still
    hanging around from a previous run.
    - Add rotateActiveFile which creates a new file to write to. This is
    used when we want to read from the current active file, or when the file
    size limit has been reached.
    
    Signed-off-by: Miaofei <miaofei@amazon.com>

---
+++ cloudwatch_logs_common/include/cloudwatch_logs_common/utils/file_manager_strategy.h
@@ -0,0 +1,58 @@
+//
+// Created by tim on 5/9/19.
+//
+
+#pragma once
+
+#include <list>
+#include "cloudwatch_logs_common/utils/task_utils.h"
+
+namespace Aws {
+namespace CloudWatchLogs {
+namespace Utils {
+
+/**
+ * Manages how files are split up, which files to write to and read when requested.
+ */
+class FileManagerStrategy {
+public:
+    FileManagerStrategy() {};
+
+    ~FileManagerStrategy() = default;
+
+    virtual void initialize();
+
+    virtual void rotateActiveFile() ;
+
+    virtual void discoverStoredFiles() ;
+
+    virtual void addFileNameToStorage(std:: string) ;
+
+    virtual uintmax_t getActiveFileSize() ;
+    /**
+     * Get the file name to write to.
+     *
+     * @return current file name
+     */
+    virtual std::string getFileToWrite() ;
+
+    virtual std::string getFileToRead();
+
+private:
+    /**
+     * Current file name to write to.
+     */
+    std::string active_file_ = "example_file.log";
+    std::list<std::string> storage_files_;
+
+    /**
+     * User configurable settings
+     */
+    std::string storage_directory_ = "/tmp/";
+    int maximum_file_size_in_bytes_ = 1024 * 1024;
+};
+
+
+}  // namespace Utils
+}  // namespace CloudwatchLogs
+}  // namespace Aws
