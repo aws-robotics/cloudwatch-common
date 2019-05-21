e136a441e10472131d2e0d9cabd27b3e1bc9c09c
Ross Desmond <44277324+ross-desmond@users.noreply.github.com>
2019-05-21 14:23:36 -0700

    Cleanup test
    Signed-off-by: Miaofei <miaofei@amazon.com>

--- cloudwatch_logs_common/test/file_management/queue_monitor_test.cpp
+++ cloudwatch_logs_common/test/file_management/queue_monitor_test.cpp
@@ -59,16 +59,20 @@ public:
 
 };
 
+bool dequeueFunc(std::string& data, std::string actual) {
+  data = std::move(actual);
+  return true;
+}
+
+using std::placeholders::_1;
+
 TEST(queue_demux_test, single_source_test)
 {
   auto observed_queue = std::make_shared<MockObservedQueue>();
   std::shared_ptr<StatusMonitor> monitor;
   std::string actual = "test_string";
-  auto dequeue_func = [actual](std::string& data) -> bool {
-    data = actual;
-    return true;
-  };
-  EXPECT_CALL(*observed_queue, dequeue(_)).WillOnce(Invoke(dequeue_func));
+  EXPECT_CALL(*observed_queue, dequeue(_))
+    .WillOnce(Invoke(std::bind(dequeueFunc, _1, actual)));
   QueueMonitor<std::string> queue_monitor;
   queue_monitor.addSink(observed_queue, PriorityOptions());
   std::string data;
@@ -78,23 +82,17 @@ TEST(queue_demux_test, single_source_test)
 
 TEST(queue_demux_test, multi_source_test)
 {
-  auto dequeue_func = [](std::string& data, std::string actual) -> bool {
-    data = actual;
-    return true;
-  };
-
   QueueMonitor<std::string> queue_monitor;
   auto low_priority_queue = std::make_shared<StrictMock<MockObservedQueue>>();
-  using std::placeholders::_1;
   EXPECT_CALL(*low_priority_queue, dequeue(_))
-    .WillOnce(Invoke(std::bind(dequeue_func, _1, "low_priority")))
+    .WillOnce(Invoke(std::bind(dequeueFunc, _1, "low_priority")))
     .WillRepeatedly(Return(false));
   queue_monitor.addSink(low_priority_queue, PriorityOptions(LOWEST_PRIORITY));
 
   auto high_priority_observed_queue = std::make_shared<StrictMock<MockObservedQueue>>();
   std::shared_ptr<StatusMonitor> monitor;
   EXPECT_CALL(*high_priority_observed_queue, dequeue(_))
-    .WillOnce(Invoke(std::bind(dequeue_func, _1, "high_priority")))
+    .WillOnce(Invoke(std::bind(dequeueFunc, _1, "high_priority")))
     .WillRepeatedly(Return(false));;
   queue_monitor.addSink(high_priority_observed_queue, PriorityOptions(HIGHEST_PRIORITY));
   std::string data;
