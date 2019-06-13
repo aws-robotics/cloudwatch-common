ff93ed12581ce037c68014319d22750552d429fc
Devin Bonnie <dbbonnie@amazon.com>
2019-06-13 12:34:13 -0700

    Addressed review comments
    Signed-off-by: Miaofei <miaofei@amazon.com>

--- cloudwatch_logs_common/include/cloudwatch_logs_common/log_batcher.h
+++ cloudwatch_logs_common/include/cloudwatch_logs_common/log_batcher.h
@@ -74,7 +74,8 @@ public:
     return this->publish_trigger_batch_size_.load();
 
   }
-  inline void resetPublishTriggerBatchSize(size_t new_value) {
+
+  inline void resetPublishTriggerBatchSize() {
       this->publish_trigger_batch_size_.store(kDefaultTriggerSize);
   }
 
@@ -157,7 +158,7 @@ public:
   virtual size_t getCurrentBatchSize() override;
 
   size_t getMaxAllowableBatchSize(); //todo part of data batcher?
-  void setMaxAllowableBatchSize(size_t new_value); //todo part of data batcher?
+  void setMaxAllowableBatchSize(size_t new_batch_size); //todo part of data batcher?
 
   bool handleSizeExceeded(); //todo part of data batcher?
 
