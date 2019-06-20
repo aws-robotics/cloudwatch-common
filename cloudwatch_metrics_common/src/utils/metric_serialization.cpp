b9f02ef8075ddd5a464dd8c1499b7ab5c8f537aa
Tim Robinson <tim@amazon.com>
2019-06-19 17:39:09 -0700

    Improve metric serialization, add tests.
    Signed-off-by: Miaofei <miaofei@amazon.com>

--- cloudwatch_metrics_common/src/utils/metric_serialization.cpp
+++ cloudwatch_metrics_common/src/utils/metric_serialization.cpp
@@ -25,6 +25,8 @@ namespace CloudWatch {
 namespace Metrics {
 namespace Utils {
 
+using JsonValue = Aws::Utils::Json::JsonValue;
+
 static constexpr const char* kTimestampKey = "timestamp";
 static constexpr const char* kMetricNameKey = "metric_name";
 static constexpr const char* kCountsKey = "counts";
@@ -35,13 +37,28 @@ static constexpr const char* kValuesKey = "values";
 static constexpr const char* kStorageResolutionKey = "storage_resolution";
 static constexpr const char* kUnitKey = "unit";
 
+static const std::vector<Aws::String> required_properties = {
+    kMetricNameKey,
+    kTimestampKey
+};
+
 Model::MetricDatum deserializeMetricDatum(const Aws::String  &basic_string) {
   Aws::String aws_str(basic_string.c_str());
-  Aws::Utils::Json::JsonValue json_value(aws_str);
+  JsonValue json_value(aws_str);
+  if (!json_value.WasParseSuccessful()) {
+    throw std::invalid_argument("Failed to parse JSON value");
+  }
   auto view = json_value.View();
+  for (const auto property : required_properties) {
+    if (!view.KeyExists(property)) {
+      std::string property_name(property.c_str());
+      throw std::invalid_argument("Could not find required property " + property_name + " in JSON data");
+    }
+  }
+
   Model::MetricDatum datum;
-  datum.SetTimestamp(view.GetInt64(kTimestampKey));
   datum.SetMetricName(view.GetString(kMetricNameKey));
+  datum.SetTimestamp(view.GetInt64(kTimestampKey));
 
   if (view.KeyExists(kCountsKey)) {
     auto array = view.GetArray(kCountsKey);
@@ -55,19 +72,33 @@ Model::MetricDatum deserializeMetricDatum(const Aws::String  &basic_string) {
   // datum.SetDimensions();
   // datum.SetStatisticValues();
   // datum.SetValues();
-  datum.SetStorageResolution(view.GetInteger(kStorageResolutionKey));
-  datum.SetUnit(Model::StandardUnit(view.GetInteger(kUnitKey)));
+  if (view.KeyExists(kStorageResolutionKey)) {
+    datum.SetStorageResolution(view.GetInteger(kStorageResolutionKey));
+  }
+  if (view.KeyExists(kUnitKey)) {
+    datum.SetUnit(Model::StandardUnit(view.GetInteger(kUnitKey)));
+  }
+
   return datum;
 }
 
 Aws::String serializeMetricDatum(const Model::MetricDatum &datum) {
   Aws::Utils::Json::JsonValue json_value;
-  // @todo (rddesmon) serialization of complex
+
+  const Aws::Vector<double> counts = datum.GetCounts();
+  Aws::Utils::Array<JsonValue> counts_array = Aws::Utils::Array<JsonValue>(counts.size());
+  for (size_t i = 0; i < counts.size(); ++i) {
+    JsonValue count_json;
+    count_json.WithDouble(kCountKey, counts[i]);
+    counts_array[i] = count_json;
+  }
+
   json_value
-    .WithInt64(kTimestampKey, datum.GetTimestamp().CurrentTimeMillis())
+    .WithInt64(kTimestampKey, datum.GetTimestamp().Millis())
     .WithString(kMetricNameKey, datum.GetMetricName())
     .WithInteger(kStorageResolutionKey, datum.GetStorageResolution())
-    .WithInteger(kUnitKey, static_cast<int>(datum.GetUnit()));
+    .WithInteger(kUnitKey, static_cast<int>(datum.GetUnit()))
+    .WithArray(kCountsKey, counts_array);
   return json_value.View().WriteCompact();
 }
 
