ebaea7454e632033d9f49e25a03353b605f49f95
Devin Bonnie <dbbonnie@amazon.com>
2019-07-03 16:53:02 -0700

    Added relevant unit tests Minor fixes and cleanup
    Signed-off-by: Miaofei <miaofei@amazon.com>

--- dataflow_lite/include/dataflow_lite/utils/waiter.h
+++ dataflow_lite/include/dataflow_lite/utils/waiter.h
@@ -12,6 +12,9 @@
  * express or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
+
+#pragma once
+
 #include <chrono>
 #include <mutex>
 
